94a228916931adf8e3b609846659810ece26b110
Valdeir Rodrigues <vardii.rodrigues@gmail.com>
2023-06-21 17:37:56 -0400

    test for generate cointegrated pairs csv file

--- program/public_requests.py
+++ program/public_requests.py
@@ -62,7 +62,7 @@ def construct_market_prices(client):
 
     # Append other proces to DataFrame
     # You can limit the amount to loop through here to save time in development
-    for market in tradeable_markets[1:5]:
+    for market in tradeable_markets[1:]:
         close_prices_add = get_candles_historical(client, market)
         df_add = pd.DataFrame(close_prices_add)
         df_add.set_index("datetime", inplace=True)
