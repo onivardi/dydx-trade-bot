100d8e8d1608a0f75413b57407ce9d44c2d42c56
Valdeir Rodrigues <vardii.rodrigues@gmail.com>
2023-06-21 10:12:51 -0400

    add market prices func

--- program/public_requests.py
+++ program/public_requests.py
@@ -3,7 +3,7 @@ from utils import get_ISO_times
 import pandas as pd
 import numpy as np
 from pprint import pprint
-
+import time
 
 # Get relevant time periods for ISO from and to
 ISO_TIMES = get_ISO_times()
@@ -43,4 +43,40 @@ def get_candles_historical(client, market):
 
 # Construct market prices
 def construct_market_prices(client):
-    pass
+    
+    
+    tradeable_markets = []
+    markets = client.public.get_markets()
+
+    # Find tradeable pairs
+    for market in markets.data["markets"].keys():
+        market_info = markets.data["markets"][market]
+        if market_info["status"] == "ONLINE" and market_info["type"] == "PERPETUAL":
+            tradeable_markets.append(market)
+
+    # Set initial DateFrame
+    close_price = get_candles_historical(client, tradeable_markets[0])
+
+    df = pd.DataFrame(close_price)
+    df.set_index("datetime", inplace=True)
+
+    # Append other proces to DataFrame
+    # You can limit the amount to loop through here to save time in development
+    for market in tradeable_markets[1:5]:
+        close_prices_add = get_candles_historical(client, market)
+        df_add = pd.DataFrame(close_prices_add)
+        df_add.set_index("datetime", inplace=True)
+        df = pd.merge(df, df_add, how="outer", on="datetime", copy=False)
+
+        # Protect Memories
+        del df_add
+
+    # Check any columns with NaNs
+    nans = df.columns[df.isna().any()].tolist()
+    if len(nans) > 0:
+        print("Dropping columns: ")
+        print(nans)
+        df.drop(columns=nans, inplace=True)
+
+    # Return result
+    return df
