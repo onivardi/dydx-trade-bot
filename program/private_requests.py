32e43a81e6ef6101d83b6bf567d79fa51d184c2f
Valdeir Rodrigues <vardii.rodrigues@gmail.com>
2023-06-21 18:38:27 -0400

    add bot agent

--- program/private_requests.py
+++ program/private_requests.py
@@ -3,6 +3,11 @@ import time
 
 from utils import format_number
 
+# Check order status
+def check_order_status(client, order_id):
+    order = client.private.get_order_by_id(order_id)
+    return order.data["order"]["status"]
+
 # Place market order
 def place_market_order(client, market, side, size, price, reduce_only):
     # Get Position
